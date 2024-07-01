17523e5986278ef4310b3dc5c2c202768a2b02b7
Areej <areejfatimasheikh25@gmail.com>
2024-07-01 18:23:42 +0530

    new changes

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
 //                          STUDENT GRADE MANAGMENT SYSTEM
 
-let student1={
-    name:"Abc",
-    id: "001",
-    grade:"B+",
-}
-const grade001={
-    subject1:70,
-    subject2:70,
-    subject3:70,
-}
-let student2={
-    name:"Bcd",
-    id: "002",
-    grade:"A+",
-}
-const grade002={
-    subject1:93,
-    subject2:93,
-    subject3:93
-}
-let student3={
-    name:"Cde",
-    id: "003",
-    grade:"B",
-}
-const grade003={
-    subject1:75,
-    subject2:75,
-    subject3:75
-}
-let student4={
-    name:"Abc",
-    id: "004",
-    grade:"A",
-}
-const grade004={
-    subject1:90,
-    subject2:90,
-    subject3:90
-}
-let student5={
-    name:"Abc",
-    id: "005",
-    grade:"A+",
-}
-const grade005={
-    subject1:96,
-    subject2:96,
-    subject3:96
-}
+let student1 = {
+  name: "Abc",
+  id: "001",
+  grade: {
+    subject1: 70,
+    subject2: 70,
+    subject3: 70,
+  },
+};
+
+let student2 = {
+  name: "Bcd",
+  id: "002",
+  grade: {
+    subject1: 93,
+    subject2: 93,
+    subject3: 93,
+  },
+};
+
+let student3 = {
+  name: "Cde",
+  id: "003",
+  grade: {
+    subject1: 75,
+    subject2: 75,
+    subject3: 75,
+  },
+};
+
+let student4 = {
+  name: "Abc",
+  id: "004",
+  grade: {
+    subject1: 90,
+    subject2: 90,
+    subject3: 90,
+  },
+};
+let student5 = {
+  name: "Abc",
+  id: "005",
+  grade: {
+    subject1: 96,
+    subject2: 96,
+    subject3: 96,
+  },
+};
+
 // console.log(student1);
 // console.log(student2);
 // console.log(student3);
@@ -62,29 +61,50 @@ const grade005={
 // console.log(grade004);
 // console.log(grade005);
 
-let student6={
-    name:"Efg",
-    id: "006",
-    grade:"B",
+let student6 = {
+  name: "Efg",
+  id: "006",
+  grade: {
+    subject1: 96,
+    subject2: 96,
+    subject3: 96,
+  },
+};
+
+let studentDetail = [
+  { student1 },
+  { student2 },
+  { student3 },
+  { student4 },
+  { student5 },
+];
+
+function add() {
+    let n = prompt("Enter Student name: ");
+    let i = prompt("Enter Student id: ");
+    let s1 = prompt("Enter Subject 1 grage")
+    let s2 = prompt("Enter Subject 2 grage")
+    let s3 = prompt("Enter Subject 3 grage")
+
+    let temp ={
+        name: n,
+        id: i,
+        grade: {
+          subject1: s1,
+          subject2: s2,
+          subject3: s3,
+        },
+    }
+
+    return studentDetail.push(temp)
 }
-
-
-
-let studentDetail=[{student1},{student2},{student3},{student4},{student5}]
+add();
 console.log(studentDetail);
 
-function add(student){
-    studentDetail =studentDetail.push({student6})
-    return studentDetail;  
+function sub() {
+    let x = prompt("kisko remove karna hai");
+    return delete studentDetail[x-1];
 }
-add()
-console.log(studentDetail);
+sub();
 
-
-
-function sub(student){
-   delete studentDetail[5]
-    return studentDetail;    
-}
-sub()
 console.log(studentDetail);
